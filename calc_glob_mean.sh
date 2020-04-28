fb0a5328fcbd9f45ae02c42d7e0117ca86149310
Vincenzo DeToma <Vincenzo.DeToma@artov.ismar.cnr.it@vdetoma-vm01.artov.isac.cnr.it>
2020-04-28 14:36:02 +0000

    Modifications to the script for global mean time series

--- calc_glob_mean.sh
+++ calc_glob_mean.sh
@@ -3,21 +3,21 @@
 basedir=/DataArchive/C3S/subsurf_sal
 var=so
 
-#for (( y=1979;y<=2018;y++ ));do
-#  mkdir -p ${basedir}/tmp/${y}
-#  for (( m=1;m<=12;m++ ));do
-#    d=1
-#    python glob_month_depth.py ${var} ${y} `printf "%02d" ${m}` ${d}
-#    d=300
-#    python glob_month_depth.py ${var} ${y} `printf "%02d" ${m}` ${d}
-#    d=700
-#    python glob_month_depth.py ${var} ${y} `printf "%02d" ${m}` ${d}
-#    d=2000
-#    python glob_month_depth.py ${var} ${y} `printf "%02d" ${m}` ${d}
-#    d=6000
-#    python glob_month_depth.py ${var} ${y} `printf "%02d" ${m}` ${d}
-#  done
-#done
+for (( y=1979;y<=2018;y++ ));do
+  mkdir -p ${basedir}/tmp/${y}
+  for (( m=1;m<=12;m++ ));do
+    d=1
+    python glob_month_depth.py ${var} ${y} `printf "%02d" ${m}` ${d}
+    d=300
+    python glob_month_depth.py ${var} ${y} `printf "%02d" ${m}` ${d}
+    d=700
+    python glob_month_depth.py ${var} ${y} `printf "%02d" ${m}` ${d}
+    d=2000
+    python glob_month_depth.py ${var} ${y} `printf "%02d" ${m}` ${d}
+    d=6000
+    python glob_month_depth.py ${var} ${y} `printf "%02d" ${m}` ${d}
+  done
+done
 d=1
 cdo mergetime ${basedir}/tmp/*/glob_${var}_${d}m_ORCA-0.25x0.25_regular_*.nc ${basedir}/Results/glob_${var}_${d}m_ORCA-0.25x0.25_regular_1979_2018.nc
 d=300
--- glob_month_depth.py
+++ glob_month_depth.py
@@ -16,12 +16,44 @@ lon = ds.lon
 Nlat = len(lat)
 Nlon = len(lon)
 depthvar = ds.depth
-idx = len(depthvar.sel(depth=slice(0, float(depth))))
+idx = len(depthvar.sel(depth=slice(0, float(depth))))+1 if depth=='300' else len(depthvar.sel(depth=slice(0, float(depth))))
+dz = dm['e3t_1d'].squeeze()
+lev=float(depth)
+dz2 = dz.copy()
+if(depth!='1'):
+  if(lev>dz[:idx].sum()):
+    nlt = len(ds.depth)
+  else:
+    zt = 0
+    for l in range(len(ds.depth)):
+      zt = zt + dz[l]
+      if(zt <lev):
+        nlt = l
+        ztf = zt
+    nlt = nlt +1
+    dz2[nlt] = lev - ztf
+
 var = ds[variable].isel(depth=slice(0,idx))
-dz = dm['e3t_1d'].isel(depth=slice(0, idx))
+dz2 = dz2.isel(depth=slice(0, idx))
 w = np.cos(lat*np.pi/180.)
 h_av = var.weighted(w).mean(dim=['lat', 'lon'])
-globmean = h_av.weighted(dz).mean(dim='depth')
+globmean = var.weighted(w*dz2).mean(dim=['lat', 'lon', 'depth'])
+print(year, month, depth, globmean.values)
+
+
+#dm = xr.open_dataset(weight_path+'/'+'mesh_mask.nc').rename({'t': 'time', 'z': 'depth'})
+#ds = xr.open_dataset(data_path+variable+'/'+variable+'_ORCA-0.25x0.25_regular_'+year+month+'.nc')
+#lat = ds.lat
+#lon = ds.lon
+#Nlat = len(lat)
+#Nlon = len(lon)
+#depthvar = ds.depth
+#idx = len(depthvar.sel(depth=slice(0, float(depth))))
+#var = ds[variable].isel(depth=slice(0,idx))
+#dz = dm['e3t_1d'].isel(depth=slice(0, idx))
+#w = np.cos(lat*np.pi/180.)
+#h_av = var.weighted(w).mean(dim=['lat', 'lon'])
+#globmean = h_av.weighted(dz).mean(dim='depth')
 
 globmean = globmean.rename(variable+depth)
 globmean.to_netcdf(dest_path+'tmp/'+year+'/glob_'+variable+'_'+depth+'m_ORCA-0.25x0.25_regular_'+year+month+'.nc')
